ceffe0b0f8a67c3891f09330268b7afb59cb5c86
Samuel Pitonak <development@samuelpitonak.com>
2025-08-20 19:06:07 +0200

    refactor: better redirect file names

--- scripts/build-redirects.js
+++ scripts/build-redirects.js
@@ -3,36 +3,19 @@
 const fs = require('fs');
 const path = require('path');
 
-// Helper function to load JSON file
 function loadRedirectsFile(filename) {
   const filePath = path.join(__dirname, '..', 'data', filename);
   return JSON.parse(fs.readFileSync(filePath, 'utf8'));
 }
 
-// Load all redirect categories from JSON files
-const githubProjects = loadRedirectsFile('github-projects.json');
-const websiteVersions = loadRedirectsFile('website-versions.json');
-const legacyManifesto = loadRedirectsFile('legacy-manifesto.json');
-const appLinks = loadRedirectsFile('app-links.json');
-const appearances = loadRedirectsFile('appearances.json');
-const content = loadRedirectsFile('content.json');
-
-// Combine all sections into the final redirects object
 const redirects = {
-  ...githubProjects,
-
-  ...websiteVersions,
-
-  ...legacyManifesto,
-
-  ...appLinks,
-
-  ...appearances,
-
-  ...content,
+  ...loadRedirectsFile('internal-oss-tools.json'),
+  ...loadRedirectsFile('legacy-websites.json'),
+  ...loadRedirectsFile('apps.json'),
+  ...loadRedirectsFile('appearances.json'),
+  ...loadRedirectsFile('content.json'),
 };
 
-// Write to redirects.json
 const outputPath = path.join(__dirname, '..', 'redirects.json');
 fs.writeFileSync(outputPath, JSON.stringify(redirects, null, 2) + '\n');
 
